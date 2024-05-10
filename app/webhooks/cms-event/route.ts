e859799676e1658956d4a1503cb875a18aff6f82
Devidev <47260520+dev634@users.noreply.github.com>
2024-05-10 12:42:15 +0200

    add ngrok for preprod mode

--- app/webhooks/cms-event/route.ts
+++ app/webhooks/cms-event/route.ts
@@ -3,6 +3,7 @@ import { revalidateTag } from "next/cache";
 
 export async function POST(request) {
 	const payload = await request.json();
+	console.log("[cms-event/routes.ts] : ", payload);
 	if (payload.model === "review") {
 		revalidateTag(CACHE_TAG_REVIEWS);
 		console.log("revalidated : ", CACHE_TAG_REVIEWS);
